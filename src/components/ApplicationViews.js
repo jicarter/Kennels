511d1cb75fa781460062ed9c3f4866a6735f34bd
Jordan Carter <jordan.carter.0023@gmail.com>
2021-01-22 08:53:44 -0600

    completed chapters

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,10 +2,12 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
 import { AnimalList } from "./animal/AnimalList"
-import { CustomerCard } from "./customer/Customer";
+import { AnimalProvider } from "./animal/AnimalProvider";
+import { CustomerProvider } from "./customer/CustomerProvider";
+import { CustomerList } from "./customer/CustomerList";
 import { LocationCard } from "./location/Location";
 import { EmployeeCard } from "./employee/Employee";
-import { AnimalProvider } from "./animal/AnimalProvider";
+
 
 export const ApplicationViews = () => {
     return (
@@ -16,26 +18,26 @@ export const ApplicationViews = () => {
             </Route>
 
             {/* Render the animal list when http://localhost:3000/animals */}
-            {/* <Route path="/animals">
-                <AnimalCard />
-            </Route> */}
+            
             <AnimalProvider>
                 <Route exact path="/animals">
                     <AnimalList />
                 </Route>
             </AnimalProvider>
             
-            {/* Render the animal list when http://localhost:3000/customer */}
-            <Route path="/customers">
-                <CustomerCard />
-            </Route>
+           
+            <CustomerProvider>
+                <Route exact path="/customers">
+                    <CustomerList />
+                </Route>
+            </CustomerProvider>
             
-            {/* Render the animal list when http://localhost:3000/location */}
+            
             <Route path="/locations">
                 <LocationCard />
             </Route>
             
-            {/* Render the animal list when http://localhost:3000/employee */}
+            
             <Route path="/employees">
                 <EmployeeCard />
             </Route>
